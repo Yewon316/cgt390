0d7376c0c88c3da00719e5f7f0bbb3e35703dbd2
Yewon316 <readchoi@naver.com>
2025-09-04 11:40:36 -0400

    first commit

---
+++ src/components/Card1.jsx
@@ -0,0 +1,11 @@
+
+const Card1 = () => {
+    const title = "Bob";
+    return (<div className ="card">
+        <h2>{title}</h2>
+        <p>Card content goes here.</p>
+    </div>
+    );
+}
+
+export default Card1;
